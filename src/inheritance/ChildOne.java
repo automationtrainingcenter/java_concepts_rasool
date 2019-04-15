3fac26916cb4ab927cdf3d250a09ec0328941da6
automation <atcsurya@gmail.com>
2019-04-15 09:15:23 +0530

    inheritance basics

---
+++ src/inheritance/ChildOne.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+public class ChildOne extends Parent {
+
+	public ChildOne() {
+		super();
+		System.out.println("ChildOne class object created");
+
+	}
+
+	public void methodTwo() {
+		System.out.println("ChildOne class method two");
+	}
+}
